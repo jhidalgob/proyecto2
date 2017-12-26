533d07c725b3df76e8e242515ff8b16a10b16ce2
jhidalgob <jhidalgob@live.com>
2017-12-26 20:08:46 -0300

    cree datos
    agregue datos a clase persona

--- EjemploGit2/src/ejemplogit2/Persona.java
+++ EjemploGit2/src/ejemplogit2/Persona.java
@@ -10,5 +10,72 @@ package ejemplogit2;
  * @author USUARIO
  */
 public class Persona {
-    
+      private String nombre;
+   private String rut;
+   private String direccion;
+   private String telefono;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String rut, String direccion, String telefono) {
+        this.nombre = nombre;
+        this.rut = rut;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", rut=" + rut + ", direccion=" + direccion + ", telefono=" + telefono + '}';
+    }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
 }
